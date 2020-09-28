a9af3f83f943cd1ba22dfccdad84081b1b0cd63f
calebdockal <64401265+calebdockal@users.noreply.github.com>
2020-09-28 14:22:17 -0400

    added redux store

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
-import {View, StyleSheet, FlatList, Alert} from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, FlatList, Alert } from 'react-native';
 import 'react-native-get-random-values';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 import AddTask from './src/screens/AddTask';
 import TaskList from './src/screens/TaskList';
+import TaskInputModal from './src/screens/TaskInputModal';
 
 const App = () => {
-  const [tasks, setTasks] = useState([{id: uuidv4(), text: 'Task 1'}]);
+  const [tasks, setTasks] = useState([{ id: uuidv4(), text: 'Task 1' }]);
 
   // To edit tasks
   const [editState, editStateChange] = useState(false);
@@ -20,6 +21,8 @@ const App = () => {
 
   const [checkedTasks, checkedTasksChange] = useState([]);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   const deleteTask = (id) => {
     setTasks((prevTasks) => {
       return prevTasks.filter((task) => task.id !== id);
@@ -30,7 +33,7 @@ const App = () => {
   const saveEditTask = (id, text) => {
     setItems((prevTasks) => {
       return prevTasks.map((task) =>
-        task.id === editTaskDetail.id ? {id, text: editTaskDetail.text} : task,
+        task.id === editTaskDetail.id ? { id, text: editTaskDetail.text } : task,
       );
     });
     editStateChange(!editState);
@@ -38,22 +41,51 @@ const App = () => {
 
   // capture text input
   const handleEditChange = (text) => {
-    editTaskDetailChange({id: editTaskDetail.id, text});
+    editTaskDetailChange({ id: editTaskDetail.id, text });
   };
   // add task
-  const addTask = (text) => {
-    if (!text) {
-      Alert.alert('Error', 'Please enter a task', {text: 'Ok'});
-    } else {
-      setTasks((prevTasks) => {
-        return [{id: uuidv4(), text}, ...prevTasks];
-      });
-    }
+  const addTask = () => {
+    /**
+     * Here you make the modal visible
+     */
+
+    setIsModalVisible(true);
+
+    // return
+    // if (!text) {
+    //   Alert.alert('Error', 'Please enter a task', { text: 'Ok' });
+    // } else {
+    //   setTasks((prevTasks) => {
+    //     return [{ id: uuidv4(), text }, ...prevTasks];
+    //   });
+    // }
   };
+
+  const insertNewTaskHandler = (task) => {
+    /**
+     * Here we capture the value typed into the text input box from the modal view
+     */
+
+    //print
+    console.log("My new task is - ", task);
+
+    //hide modal dialog
+    setIsModalVisible(false);
+
+    /**
+     * Insert task into redux store via payload: {task: task}
+     */
+
+  }
+
+  const cancelNewTaskHandler = () => {
+    //hide modal dialog
+    setIsModalVisible(false);
+  }
 
   // Edit item id capture
   const editTask = (id, text) => {
-    editTaskDetailChange({id, text});
+    editTaskDetailChange({ id, text });
     return editStateChange(!editState);
   };
 
@@ -64,19 +96,23 @@ const App = () => {
     );
     isChecked.length
       ? checkedTasksChange((prevTasks) => {
-          return [...prevTasks.filter((task) => task.id !== id)];
-        })
+        return [...prevTasks.filter((task) => task.id !== id)];
+      })
       : checkedTasksChange((prevTasks) => {
-          return [...prevTasks.filter((task) => task.id !== id), {id, text}];
-        });
+        return [...prevTasks.filter((task) => task.id !== id), { id, text }];
+      });
   };
 
   return (
     <View style={styles.container}>
-      <AddTask addTask={addTask} />
+      {/**
+         * It is common convention to name a prop that does an action like so, "onAddTask"
+         * Other examples are: onPress, onTextChange, etc.
+         */}
+      <AddTask onAddTask={addTask} />
       <FlatList
         data={tasks}
-        renderItem={({item}) => (
+        renderItem={(item) => (
           <TaskList
             task={item}
             deleteTask={deleteTask}
@@ -89,6 +125,9 @@ const App = () => {
           />
         )}
       />
+      <TaskInputModal visible={isModalVisible}
+        onInsertNewTask={insertNewTaskHandler}
+        onCancelNewTask={cancelNewTaskHandler} />
     </View>
   );
 };
---
+++ src/redux/actions/actions.js
@@ -0,0 +1,6 @@
+import {ADD_TASK} from '../reducers';
+
+export const addTask = (task) => ({
+  type: ADD_TASK,
+  payload: item,
+});
